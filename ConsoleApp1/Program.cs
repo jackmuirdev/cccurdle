344446470bfc821243cf57c8ea90fee8e133ab10
Jack Muir <jmuirbusiness@gmail.com>
2023-11-23 13:15:44 +0000

    console colours, functionality, comments

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,57 +1,86 @@
-﻿// Introduction
-// Say hello welcome to CCCurdle
-// Instructions to play game
-
-// CPU picks random word form library
-
-// Split string into characters
-
-// Say input your guess
-
-// User inputs guess
-// Checks guess is 5 chars
-// Checks guess is an char
-// If int then print Invalid entry try again!
-// Cant continue without an 5 char input
-
-// Split string into char
-
-// Compare each char to cpu string 
-
-// If char == char then highlight green 
-// If char == char but in different space highlight yellow
-// If char != char highlight red
-
-// loop the last few lines of code
-
-// Get 6 guesses
-
-// If user guess == cpu guess print You guessed correctly, well done! End
-// After user guess print Guess Again!
-// Print how many guesses left
-// If user guesses =< 0 print Sorry you ran out of guesses - correct word is ... End
-
-// Ask user to play again (y/n)
-
-using System;
-using System.Linq;
-
-class WordleGame
+﻿class WordleGame
 {
-    static void Main()
+    // Introduction
+    // Say hello welcome to CCCurdle
+    // Instructions to play the game
+    static void Introduction()
     {
         Console.WriteLine("Welcome to Wordle!");
         Console.WriteLine("Try to guess the secret word.");
+        Console.WriteLine("Enter a 5-letter word");
+    }
 
+    // CPU picks a random word from the library
+    static string GenerateSecretWord()
+    {
+        string[] words = { "apple", "grape", "melon", "peach" };
+        Random random = new Random();
+        return words[random.Next(words.Length)];
+    }
+
+    // User inputs a guess
+    // Checks if the guess is 5 chars
+    // Checks if the guess is a char
+    // If int then print Invalid entry try again!
+    // Can't continue without a 5-char input
+    static string GetPlayerGuess()
+    {
+        Console.Write("Enter your guess: ");
+        return Console.ReadLine().ToLower();
+    }
+
+static void DisplayGuessFeedback(string secretWord, string guess)
+{
+    // Compare each char to CPU string
+    for (int i = 0; i < secretWord.Length; i++)
+    {
+        // If char == char then highlight green
+        if (secretWord[i] == guess[i])
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(guess[i] + " ");
+        }
+        // If char == char but in a different space highlight yellow
+        else if (secretWord.Contains(guess[i]))
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(guess[i] + " ");
+        }
+        // If char != char highlight red
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(guess[i] + " ");
+        }
+    }
+
+    Console.ForegroundColor = ConsoleColor.White; // Reset console color
+    Console.WriteLine();
+}
+
+
+
+    // Main Code
+    static void Main()
+    {
+        // Introduction
+        // Say hello welcome to CCCurdle
+        // Instructions to play the game
+        Introduction();
+
+        // CPU picks a random word from the library
         string secretWord = GenerateSecretWord();
+        // Get 6 guesses
         int maxAttempts = 6;
         int attempts = 0;
 
+        // After the user guess, print Guess Again!
         while (attempts < maxAttempts)
         {
             Console.WriteLine($"Attempt {attempts + 1}/{maxAttempts}");
             string guess = GetPlayerGuess();
 
+            // If the user guess == CPU guess print You guessed correctly, well done! End
             if (guess == secretWord)
             {
                 Console.WriteLine("Congratulations! You guessed the correct word!");
@@ -62,48 +91,13 @@ class WordleGame
             attempts++;
         }
 
+        // If user guesses <= 0 print Sorry you ran out of guesses - the correct word is ... End
         if (attempts == maxAttempts)
         {
             Console.WriteLine($"Sorry, you've run out of attempts. The correct word was: {secretWord}");
         }
 
         Console.ReadLine(); // Pause before exiting
-    }
-
-    static string GenerateSecretWord()
-    {
-        string[] words = { "apple", "banana", "orange", "grape", "melon", "peach" };
-        Random random = new Random();
-        return words[random.Next(words.Length)];
-    }
-
-    static string GetPlayerGuess()
-    {
-        Console.Write("Enter your guess: ");
-        return Console.ReadLine().ToLower();
-    }
-
-    static void DisplayGuessFeedback(string secretWord, string guess)
-    {
-        char[] feedback = new char[secretWord.Length];
-
-        for (int i = 0; i < secretWord.Length; i++)
-        {
-            if (secretWord[i] == guess[i])
-            {
-                feedback[i] = 'O'; // Correct letter in the correct position
-            }
-            else if (secretWord.Contains(guess[i]))
-            {
-                feedback[i] = 'X'; // Correct letter in the wrong position
-            }
-            else
-            {
-                feedback[i] = '_'; // Incorrect letter
-            }
-        }
-
-        Console.WriteLine("Feedback: " + new string(feedback));
-        Console.WriteLine();
+        // Ask the user to play again (y/n)
     }
 }
